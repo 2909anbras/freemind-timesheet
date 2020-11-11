b100be0e866fd8e73fcbf5db4063284292e46c55
2909anbras <72259908+2909anbras@users.noreply.github.com>
2020-11-11 19:14:20 +0100

    Revert "Jhipster entities test 62991575280215"

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,26 +4,6 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
-      {
-        path: 'app-user',
-        loadChildren: () => import('./app-user/app-user.module').then(m => m.FreemindTimesheetAppUserModule),
-      },
-      {
-        path: 'company',
-        loadChildren: () => import('./company/company.module').then(m => m.FreemindTimesheetCompanyModule),
-      },
-      {
-        path: 'customer',
-        loadChildren: () => import('./customer/customer.module').then(m => m.FreemindTimesheetCustomerModule),
-      },
-      {
-        path: 'project',
-        loadChildren: () => import('./project/project.module').then(m => m.FreemindTimesheetProjectModule),
-      },
-      {
-        path: 'job',
-        loadChildren: () => import('./job/job.module').then(m => m.FreemindTimesheetJobModule),
-      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
