cb7a03a57d5e670d5702c2c2bdf30ebccf63e9e3
2909anbras <72259908+2909anbras@users.noreply.github.com>
2021-01-07 11:37:00 +0100

    avancement timesheet

---
+++ src/main/webapp/app/timesheet/timesheet.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { SERVER_API_URL } from 'app/app.constants';
+import { createRequestOption } from 'app/shared/util/request-util';
+
+type EntityResponseType = HttpResponse<any>;
+// type EntityArrayResponseType = HttpResponse<ICompany[]>;
+
+@Injectable({ providedIn: 'root' })
+export class TimesheetService {
+  public resourceUrl = SERVER_API_URL + 'api/report';
+
+  constructor(protected http: HttpClient) {}
+
+  create(date: Date, userId: number): Observable<HttpResponse<any>> {
+    console.log('DEDANS LE SERVICE');
+    return this.http.post<any>(`${this.resourceUrl + '/fullReport'}/${userId}`, date, { observe: 'response' });
+  }
+}
