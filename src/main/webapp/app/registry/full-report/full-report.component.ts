5e0b22af94e252108f3655b008ff903b1cc1440a
2909anbras <72259908+2909anbras@users.noreply.github.com>
2021-01-17 14:31:23 +0100

    init template full report

---
+++ src/main/webapp/app/registry/full-report/full-report.component.ts
@@ -0,0 +1,108 @@
+import { Component, OnInit } from '@angular/core';
+import { JhiEventManager } from 'ng-jhipster';
+import { JhiLanguageService } from 'ng-jhipster';
+import { SessionStorageService } from 'ngx-webstorage';
+import { HttpResponse } from '@angular/common/http';
+import { Directive, ElementRef, Renderer2, Input } from '@angular/core';
+
+import { ITEMS_PER_PAGE } from 'app/shared/constants/pagination.constants';
+import { Subscription, combineLatest } from 'rxjs';
+import { ICompany } from 'app/shared/model/company.model';
+import { CompanyService } from 'app/entities/company/company.service';
+import { ICustomer } from 'app/shared/model/customer.model';
+
+import { AppUserService } from 'app/entities/app-user/app-user.service';
+import { IAppUser } from 'app/shared/model/app-user.model';
+import { Account } from 'app/core/user/account.model';
+import { AccountService } from 'app/core/auth/account.service';
+import { IProject } from 'app/shared/model/project.model';
+
+@Component({
+  selector: 'jhi-full-report',
+  templateUrl: './full-report.component.html',
+})
+export class FullReportComponent implements OnInit {
+  showCompanies: ICompany[] = [];
+  allCompanies: ICompany[] = [];
+
+  userHidden = false;
+  companyHidden = false;
+  customerHidden = false;
+  jobHidden = false;
+  projectHidden = false;
+
+  searchUser = '';
+  searchCompany = '';
+  searchCustomer = '';
+  searchJob = '';
+  searchProject = '';
+
+  searchCustomerState = 'All';
+  searchUserState = 'All';
+  searchProjectState = 'All';
+  searchJobState = 'All';
+  searchCompanyState = 'All';
+
+  enabledStateList = ['Enable', 'Disabled', 'All'];
+
+  currentAccount: Account | null = null;
+
+  constructor(
+    protected companyService: CompanyService,
+    protected appUserService: AppUserService,
+    protected accountService: AccountService,
+    protected eventManager: JhiEventManager
+  ) {}
+
+  ngOnInit(): void {
+    this.accountService.identity().subscribe(account => (this.currentAccount = account));
+
+    // if (this.currentAccount && !this.accountService.hasAnyAuthority('ROLE_ADMIN'))
+    if (this.currentAccount)
+      if (this.accountService.hasAnyAuthority('ROLE_ADMIN')) {
+        this.companyService.query().subscribe((res: HttpResponse<ICompany[]>) => {
+          if (res.body) {
+            this.showCompanies = res.body;
+            this.allCompanies = res.body;
+            this.sortingCompanies();
+            console.log(this.showCompanies);
+          }
+        });
+      } else {
+        this.appUserService.find(this.currentAccount.id).subscribe((res: HttpResponse<IAppUser>) => {
+          this.currentAccount && res.body ? (this.currentAccount.companyId = res.body.companyId) : null;
+          console.log(this.currentAccount);
+          if (this.currentAccount?.companyId)
+            this.companyService.find(this.currentAccount?.companyId).subscribe((res: HttpResponse<ICompany>) => {
+              res.body ? this.allCompanies?.push(res.body) : null;
+              this.showCompanies = this.allCompanies;
+              console.log(this.showCompanies);
+            });
+        });
+      }
+  }
+  private sortingCompanies(): void {
+    this.showCompanies.sort((a, b) => {
+      if (a.name && b.name) {
+        if (a.name.localeCompare(b.name) === 1) {
+          return 1;
+        } else if (a.name.localeCompare(b.name) === -1) {
+          return -1;
+        } else return 1;
+      } else return 1;
+    });
+  }
+
+  public switchCompanyHidden(): void {
+    this.companyHidden = !this.companyHidden;
+  }
+  public switchCustomerHidden(): void {
+    this.customerHidden = !this.customerHidden;
+  }
+  public switchProjectHidden(): void {
+    this.projectHidden = !this.projectHidden;
+  }
+  public switchUserHidden(): void {
+    this.userHidden = !this.userHidden;
+  }
+}
